d2cdb580f3fab8e08f38e80f3214a4eee27c73e0
onda2me <onda2me@naver.com>
2021-09-24 16:26:09 +0900

    Update Kakao17Ques2.java

--- src/com/onda2me/algorithm/kakao/Kakao17Ques2.java
+++ src/com/onda2me/algorithm/kakao/Kakao17Ques2.java
@@ -30,13 +30,13 @@ package com.onda2me.algorithm.kakao;
 
 # 입출력 예제
 예제	dartResult	answer	설명
-1	1S2D*3T	37	1^1 * 2 + 2^2 * 2 + 3^3
-2	1D2S#10S	9	1^2 + 2^1 * (-1) + 10^1
-3	1D2S0T	3	1^2 + 2^1 + 0^3
-4	1S*2T*3S	23	1^1 * 2 * 2 + 2^3 * 2 + 3^1
-5	1D#2S*3S	5	1^2 * (-1) * 2 + 2^1 * 2 + 3^1
-6	1T2D3D#	-4	1^3 + 2^2 + 3^2 * (-1)
-7	1D2S3T*	59	1^2 + 2^1 * 2 + 3^3 * 2
+1	    1S2D*3T	    37	    1^1 * 2 + 2^2 * 2 + 3^3
+2	    1D2S#10S	9	    1^2 + 2^1 * (-1) + 10^1
+3	    1D2S0T	    3	    1^2 + 2^1 + 0^3
+4	    1S*2T*3S	23	    1^1 * 2 * 2 + 2^3 * 2 + 3^1
+5	    1D#2S*3S	5	    1^2 * (-1) * 2 + 2^1 * 2 + 3^1
+6	    1T2D3D#	    -4	    1^3 + 2^2 + 3^2 * (-1)
+7	    1D2S3T*	    59	    1^2 + 2^1 * 2 + 3^3 * 2
 
 */
 public class Kakao17Ques2 {
@@ -45,8 +45,8 @@ public class Kakao17Ques2 {
     public static void main(String[] args) {
 
         // 입력정보 변수선언
-		String question[] = {"1S2D*3T", "1D2S#10S", "1D2S0T", "1S*2T*3S", "1D#2S*3S", "1T2D3D#", "1D2S3T*"};
-		String answer[] = {"37", "9", "3", "23", "5", "-4", "59"};
+        String question[] = {"1S2D*3T", "1D2S#10S", "1D2S0T", "1S*2T*3S", "1D#2S*3S", "1T2D3D#", "1D2S3T*"};
+        String answer[] = {"37", "9", "3", "23", "5", "-4", "59"};
 
         // 출력정보 변수선언
         int result = 0;
