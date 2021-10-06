93bb012f31de5300ef604e9fbf45140f04b6ec8e
onda2me <onda2me@naver.com>
2021-10-06 13:43:09 +0900

    update

--- src/com/onda2me/algorithm/step2/String07DigitConversion.java
+++ src/com/onda2me/algorithm/step2/String07DigitConversion.java
@@ -2,43 +2,54 @@ package com.onda2me.algorithm.step2;
 /*
 진법 변환
 
-원본 문제 : https://www.acmicpc.net/problem/1769
-
-# 문제
+### 문제
 10진법 수 N이 주어진다. 이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
 10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
 A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
 
-# 입력
+### 입력
 첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36) N은 10억보다 작거나 같은 자연수이다.
 
-# 출력
+### 출력
 첫째 줄에 10진법 수 N을 B진법으로 출력한다.
 
 
+### 예제    
+
+입    력 |  출    력     
+----- | -----  
+10 2 | 1010
+12 8 | 14
+12 16 | C
+43 16 | 2B
+
+
+### 문제원본    
+[question_url](question_url){: target="_blank"}
+[question_url]: https://www.acmicpc.net/problem/11005
+
 */
 
-public class DigitConversion {    
+public class String07DigitConversion {    
    
     public static void main(String[] args) {
 
         // 입력정보
-        solution(2, 10, "1010"); 
-        solution(8, 12, "14");   
-        solution(16, 12, "C");   
-        solution(16, 43, "2B");        
+        solution(10, 2, "1010"); 
+        solution(12, 8, "14");   
+        solution(12, 16, "C");   
+        solution(43, 16, "2B");        
     }
-
 
     /**
      * 
-     * 주어진 10진수 숫자 num 을 n진수 수로 변환한다.
+     * 주어진 10진수 숫자 num 을 b진수 수로 변환한다.
      * 출력값 : 10(10) -> 1010(2)                iscorrect? true
-     * @param n         변경하는 진법
      * @param num       10진수 숫자
+     * @param b         변경하는 진법
      * @param answer    n진수 변환 값
      */
-    private static void solution(int n, int num, String answer) {
+    private static void solution(int num, int b, String answer) {
 
         String str = "";
         int remainder = 0;
@@ -46,21 +57,20 @@ public class DigitConversion {
         System.out.print(num +"(10) -> ");
 
         while(num  != 0) {
-            remainder = num%n;
+            remainder = num%b;
 
             if(remainder < 10) {
-                str = remainder + str;
-            
+                str = remainder + str;            
             }
             else 
             {
                 str = ((char)(remainder+55)) + str;
             }
-            num = num/n;
+            num = num/b;
             
         }
 
-        System.out.println(str + "("+n+")  \t\t iscorrect? " + (answer.equals(str.toUpperCase())));
+        System.out.println(str + "("+b+")  \t\t iscorrect? " + (answer.equals(str.toUpperCase())));
     }
 }
 
